c50b93a8ac50a07f6de83f7499c002c22305d0ed
Kang Junmo <rapier83@gmail.com>
2018-07-12 05:46:35 +0900

    why is it running? I don't konw * The error was: Type Error ... view must be callable ...

--- Payment/views.py
+++ Payment/views.py
@@ -1,17 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index():
-    # return render(request, '<html><body>This is index.</body></html>', {})
-    return HttpResponse('<html><body>This is index.</body></html>')
-
-
-def paymentMain():
+def paymentMain(request):
     # return render(request, '<html><body>This is main.</body></html>', {})
     return HttpResponse('<html><body>This is pay. main.</body></html>')
-
-
-def detail():
-    # return render(request, '<html><body>404 Error. Page Not Found.</body></html>', {})
-    return HttpResponse('<html><body>This is pay. 404.</body></html>')
