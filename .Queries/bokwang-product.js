4fe7cde70bd1523823217c813ccd0f3e8b458618
KEATON-RYZEN\rapie <rapier83@gmail.com>
2018-07-17 11:14:40 +0900

    it Cannot run for "template." where should be it? the project source root or the app root?

---
+++ .Queries/bokwang-product.js
@@ -0,0 +1,31 @@
+MasterSpec = [{
+	"Unit": "Inch",
+	"Manifold": {"Variables": ["Length", "Height", "Thickness","Depth", "Size", "Width"],
+				 "Height": 1.50,
+				 "Size": 0.75, 
+				 "Depth": MasterSpec["Manifold"]["Size"]},
+	"Fin": {"Height": MasterSpec["Height"] + 0.06,
+			""
+	},
+
+
+
+}]
+
+MasterSpec["Manifold"]["Length"] = MasterSpec["Manifold"]["width"] 
+
+
+
+Finshields = [{
+	"Name": { "Number": "2744",
+			  "Prefix": "REL",
+			  "Surfix": "G"},
+	"Manifold": { "Height": 1.5,
+				  "Length": 6.038
+				  "NumberOfSlots": 13,
+				  "FinSpacing": 0.509},
+	"Fin": { "Length": 3.70,
+			 "Height": 1.50,
+			 "Thickness": 0.140},
+
+}]
