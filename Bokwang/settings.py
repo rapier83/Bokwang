a92368b565d002c1201acc116066cc5b4f544e9e
Kang Junmo <rapier83@gmail.com>
2018-07-19 02:57:08 +0900

    Success load tmplt. it would be in { app_name }/templates... unlike book. To-Do: use base file with attaching content file

--- Bokwang/settings.py
+++ Bokwang/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'grappelli',
     # Core Apps
     'django.contrib.admin',
     'django.contrib.auth',
--- Payment/models.py
+++ Payment/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.db.models import EmailField, CharField, DateField, BooleanField
 from django.core.files.storage import FileSystemStorage
@@ -6,32 +8,49 @@ import django.utils
 RequestStorage = FileSystemStorage()
 StorageLocation = '/Payment/Storage/Orders'
 
-# Scenario
-# 1. check email
-# 2. check header?
-
 
 # Mark : - 1. Orders
 
 class Order(models.Model):
-    SenderEmail   : EmailField = models.EmailField()
-    OrderedCompany: CharField = models.CharField(max_length=50)
-    ThePaper      : CharField = models.FileField(upload_to='Order Request', storage=RequestStorage,
-                                                blank=False, null=False)
-    DeliveredDate : DateField    = models.DateField(null=False)
-    Confirmed_date: DateField    = models.DateField(null=False)
-    LastUpload    : DateField    = models.DateField(default=django.utils.timezone.now, blank=True, null=False)
-    isStored      : BooleanField = models.BooleanField(default=False, null=False)
-
-    def SetLocation(self):
-        self.FollowLocation = self.DeliveredDate.clone()
 
     def __str__(self):
-        return self.title
+        """Orders are two kinds. First is Purchase order, it's from Our client-Plant Kimcheon, OC Korea- or oversee.
+         So, thar need to be archived, and don't make miss. Specially, Like missing order, late check make a catastrophic
+         accident"""
 
     def __init__(self):
-        pass
+        self.SaveLocation = None
+
+    class Paper:
+        # Model Entries(Product).
+        # Recommend write the Optional.
+        # Mark: - Scenario
+        ## 1. input PDF file receive from the orderer
+        ## 2. check it's parserable.
+        ## 3. If it is, input Automatically
+        ## 4. If it is NOT, it should input Manually, (Almost entries are Not Null)
+        ## 5. Alert User input manually
+        ## 6. Check another user, notice by e-mail or MMS
 
 
-# Mark : - 2. Products
+        def SetLocation(self):
+            self.SaveLocation = StorageLocation + '/' + self.DeliveredDate.clone()
+
+        def DateCheck(self, t):
+            def isFirst(self, b):
+                self.ReadFirst = (t, b)
+                return self.ReceivedDate
+
+            return isFirst(True)
+
+
+        SenderEmail: EmailField = models.EmailField()
+        OrderedCompany: CharField = models.CharField(max_length=50)
+        ThePaper: CharField = models.FileField(upload_to='Order Request', storage=RequestStorage,
+                                               blank=False, null=False)
+        DeliveredDate: DateField = models.DateField(null=False)
+        Confirmed_date: DateField = models.DateField(null=False)
+        LastUpload: DateField = models.DateField(default=django.utils.timezone.now, blank=True, null=False)
+        isStored: BooleanField = models.BooleanField(default=False, null=False)
+
 
